954af3fa0f5071c81ee2b9ab5c26d2dfacc2cd24
Blank <472452499@qq.com>
2018-09-26 11:37:42 +0800

    加入显示信息的功能

---
+++ app/src/main/java/com/test/coolweather/gson/Weather.java
@@ -0,0 +1,21 @@
+package com.test.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+public class Weather {
+
+    public String status;
+
+    public Basic basic;
+
+    public AQI aqi;
+
+    public Now now;
+
+    public com.test.coolweather.gson.Suggestion suggestion;
+
+    @SerializedName("daily_forecast")
+    public List<Forecast> forecastList;
+
+}
